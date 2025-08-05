9fb6d950c7f03d7c6942dacb411bc051d328a417
Nhom <nathanthommusic@gmail.com>
2025-08-06 00:54:18 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyD0u7UjVX6ug9pA71PtIGUhQ8MQ1K2e-OQ",
   authDomain: "memomu-15179.firebaseapp.com",
   projectId: "memomu-15179",
-  storageBucket: "memomu-15179.firebasestorage.app",
+  storageBucket: "memomu-15179.appspot.app",
   messagingSenderId: "569095051369",
   appId: "1:569095051369:web:762b8db6b9f1a987ac2af8"
 };
